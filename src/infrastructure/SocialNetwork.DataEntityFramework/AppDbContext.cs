413d7e5ed5145a62b712f945dbe31a783340a8db
Voro_to54 <Voro_to54@itstepru.onmicrosoft.com>
2024-03-06 21:45:32 +0400

    Добавлен класс AppDbContext c таблицами Accounts

---
+++ src/infrastructure/SocialNetwork.DataEntityFramework/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Domain.Entities;
+
+namespace SocialNetwork.DataEntityFramework
+{
+    public class AppDbContext : DbContext
+    {
+        DbSet<Account> Accounts => Set<Account>();
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+    }
+}
